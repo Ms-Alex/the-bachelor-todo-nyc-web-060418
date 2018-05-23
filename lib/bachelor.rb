196e9fa95380ed8afa9d0a26eb1e0b6ceaffc488
Alexandra Hernandez <ms.hernandeza1@gmail.com>
2018-05-23 19:23:10 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -50,7 +50,7 @@ def get_average_age_for_season(data, season)
   data.each do |season_num, season_info|
     if season_num == season
       season_info.each do |contestant|
-        contestant["age"].each { |data| age << data.to_i }
+        contestant.each { |category, data| age << data.to_i if category == "age" }
       end
     end
   end
